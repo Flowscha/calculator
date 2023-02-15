66066e25be2d5a63b9194dd60e3727a36fb4ad34
Flowscha <124140196+Flowscha@users.noreply.github.com>
2023-02-15 11:41:30 +0100

    added key math functionality

--- bla.js
+++ bla.js
@@ -1,18 +1,38 @@
 const buttons = document.querySelectorAll("button");
-const display = document.querySelector(".display");
+const zahlen = document.querySelectorAll(".zahl");
+const display = document.querySelector(".outputNumbers");
+const inputNumbers = document.querySelector(".inputNumbers")
+const result = document.querySelector(`.operator[data-key="Enter"]`)
 
-//buttton-hover anfang
+let input1 = "";
+let input2 = "";
+let resultOutput = "";
+let operatorInput = "";
+
+function calculation(a,b, operator) {
+  a = Number(a);
+  b = Number(b);
+
+  switch(operator) {
+    case "+":
+      return a+b;
+      break;
+    case "-":
+      return a-b;
+      break;
+    case "*":
+      return a*b;
+      break;
+    case "/":
+      return a/b;
+      break;
+  }
+}
+
 function removeTransistion(e) {
     if (e.propertyName !== "transform") return;
     this.classList.remove("active");
   }
-
-  
-buttons.forEach(button => button.addEventListener("mouseover", () => button.classList.add("active")));
-
-//button-hover ende
-
-display.textContent = "1984 - 1337 = 47";
 
 function zahlKlick(e) {
     const key = document.querySelector(`.zahl[data-key="${e.key}"]`);
@@ -20,17 +40,45 @@ function zahlKlick(e) {
         return;
     }
     key.classList.add("active");
-  }
+    input1 += parseInt(key.textContent);
+    
+    inputNumbers.textContent = `${input2} ${operatorInput} ${input1}`;
+  };
 
-  function operatorKlick(e) {
-    const key = document.querySelector(`.operator[data-key="${e.key}"]`);
-    if (key === null){
-        return;
-    }
-    key.classList.add("active");
+function operatorKlick(e) {
+  const key = document.querySelector(`.operator[data-key="${e.key}"]`);
+  if (key === null){
+      return;
   }
+  key.classList.add("active");
+  if (e.key === "Enter"){
+    return;
+  }
+  if (e.key === "Backspace"){
+    inputNumbers.textContent = "C L E A R"
+    display.textContent = "";
+    location.reload();
+  }else{
+  operatorInput = e.key;  
+  input2 = input1;
+  input1 = "";
+  inputNumbers.textContent = `${input2} ${operatorInput}`;
+}};
  
   buttons.forEach(button => button.addEventListener('transitionend', removeTransistion));
-  
+  buttons.forEach(button => button.addEventListener("click", () => button.classList.add("active")));
+
   window.addEventListener('keydown', zahlKlick);
   window.addEventListener('keydown', operatorKlick);
+
+  result.addEventListener('transitionend', () => {
+    resultOutput = calculation(input2, input1, operatorInput);
+    if (resultOutput % 1 != 0){
+      resultOutput = Number(resultOutput).toFixed(4);
+      resultOutput = parseFloat(resultOutput);
+    }
+    
+    display.textContent = `= ${resultOutput}`;
+  });
+  
+  
